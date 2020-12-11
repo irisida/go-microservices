850571ea66fdeaf12ee61cbd06756f0de722ccab
irisida <20445648+irisida@users.noreply.github.com>
2020-12-11 18:59:32 +0000

    verification of service controller changes - checkyerself before you wreck yerself

--- 06-githubapi/src/api/app/url-mappings.go
+++ 06-githubapi/src/api/app/url-mappings.go
@@ -1,11 +1,11 @@
 package app
 
 import (
-	"github.com/irisida/go-microservices/06-githubapi/src/api/controllers/happywave"
+	"github.com/irisida/go-microservices/06-githubapi/src/api/controllers/checkyerself"
 	"github.com/irisida/go-microservices/06-githubapi/src/api/controllers/repositories"
 )
 
 func mapUrls() {
-	router.GET("/arewegood", happywave.HappyWave)
+	router.GET("/checkyerself", checkyerself.WreckYerself)
 	router.POST("/repositories", repositories.CreateRepo)
 }
